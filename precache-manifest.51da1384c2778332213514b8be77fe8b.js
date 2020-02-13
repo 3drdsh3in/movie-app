f17ddc9aa226ff7ed2051208dbd80b409743eee8
Edward <edwrdshen@gmail.com>
2020-02-13 15:28:51 +1100

    Updates

---
+++ precache-manifest.51da1384c2778332213514b8be77fe8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271274c21c0ee1d3370863a5a42069b7",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "e276ba7900fa08c09660",
+    "url": "/movie-app/static/css/2.26e3a113.chunk.css"
+  },
+  {
+    "revision": "c3ed738e390d22177b7f",
+    "url": "/movie-app/static/css/main.faa9bd65.chunk.css"
+  },
+  {
+    "revision": "e276ba7900fa08c09660",
+    "url": "/movie-app/static/js/2.41dfe47e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-app/static/js/2.41dfe47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ed738e390d22177b7f",
+    "url": "/movie-app/static/js/main.07c20b54.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
